792150e53a14e186893f843f707cbf0f1faf7cfa
Juan José Rangel <juanjose840@yahoo.com.co>
2018-04-10 00:14:21 -0500

    0.2

--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,18 @@
 
-public class Persona {
+public abstract class Persona {
 
+	abstract public int getEdad();
+	
+	abstract public void setEdad(int edad);
+	
+	abstract public int getAltura();
+	
+	abstract public void setAltura(int alt);
+	
+	abstract public void setApellidos(String apellidos);
+	
+	abstract public String getNombreC();
+	
+	abstract public void setNombres(String nombres);
+	
 }
